40fd370e3dd1be309c80dc7774e67ac35ba4fa24
Phelipe com Ph <phelipecomph42@gmail.com>
2020-07-23 12:58:32 -0300

    Aula 2
    Passando dados para dentro do Template

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+    class Jogo:
+        def __init__(self, nome, categoria, console):
+            self.nome = nome
+            self.categoria = categoria
+            self.console = console
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
 
-app.run(host='127.0.0.1', port=8000)
+    lista = [Jogo("Super Mario", 'Aventura', 'SNES'),
+             Jogo('Super Fire', 'RPG', 'GBA')]
+    return render_template('lista.html', titulo="Jogos",
+                                        jogos=lista)
+
+app.run(host='127.0.0.1', port=8000)                                                        

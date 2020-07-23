361c3f24c20363382f5df02c4613c2139634f891
Phelipe com Ph <phelipecomph42@gmail.com>
2020-07-23 13:57:16 -0300

    Aula 4
    Usando Redirect
    Usando bootstrap
    Usando block do Jinja2 (atraves do Flask)

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 class Jogo:
@@ -21,12 +21,11 @@ def novo():
 
 @app.route('/criar', methods=['POST']) #POST
 def criar():
-    nome = request.form['nome']
+    nome = request.form['nome'] 
     categoria = request.form['categoria']
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return render_template('lista.html', titulo="Jogos",
-                                        jogos=lista)
+    return redirect('/')
 
-app.run(host='127.0.0.1', port=8000)#, debug=True)                                                        
+app.run(host='127.0.0.1', port=8000, debug=True)                                                        
